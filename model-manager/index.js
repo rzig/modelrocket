34fa5d6d436b2f999ec404a58f4509c1c6b2ebb9
Jacob Stolker <stolkerj@yahoo.com>
2023-02-25 17:10:29 -0600

    oauth support

--- model-manager/index.js
+++ model-manager/index.js
@@ -1,10 +1,26 @@
+const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
+
+const fs = require('fs');
+const os = require('os');
 const express = require('express');
 const mongodb = require('mongodb');
 const uuid = require('uuid');
 const dotenv = require("dotenv");
+const bodyParser = require('body-parser');
+const multer  = require('multer');
+
+dotenv.config();
+
+const initObjectStoreClient = require("@relaycorp/object-storage").initObjectStoreClient;
+console.log(process.env)
+const s3client = initObjectStoreClient("minio", "http://localhost:9000", process.env.ACCESS_KEY_ID, process.env.SECRET_ACCESS_ID, false);
+
+const upload = multer({ dest: os.tmpdir() });
 
 const server = express();
-dotenv.config();
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true })); 
+
 let client = null;
 let db = null;
 
@@ -16,40 +32,56 @@ const setup = async () => {
 const generateRecord = async (n, i) => {
     const template = {
         uuid:uuid.v4(),
+        key:uuid.v4(),
         name:n,
         input: {
             type: i.type,
-            size: i.size
-        },
-        key: uuid.v1(),
-        shard: {
-            ids: ['0']
+            shape: i.shape
         }
+        // shard: {
+        //     ids: ['0']
+        // }
     };
     return template;
 };
 
+const uploadToS3 = (path, uuid) => {
+    const f = fs.readFileSync(path);
+    return s3client.putObject({body: f}, uuid, "models");
+}
+
+
+server.get('/', async (req, res) => {
+    const whatever = await db.collection('models').findOne({ name: "test" })
+    res.send(whatever);
+});
+server.post('/load_model', upload.single('model'), async function (req, res) {
+    const collection = await db.collection('models')
+    const request = req.body;
+    console.log(req.body);
+    
+    if (req.body == null || req.body.name == null || req.body.input == null || req.body.input.type == null || req.body.input.shape == null) {
+        console.log("req contains NULL values, rejecting query");
+        return res.status(500);
+    }
+    const new_record = await generateRecord(request.name, request.input);
+    collection.insertOne(new_record);
+    return res.json({ key: new_record.key });
+});
+
+server.post('/upload_model_file/:model_key', upload.single('model'), async (req, res) => {
+    const file = req.file;
+    console.log(file);
+    console.log(typeof file.path);
+    const model_key = req.params.model_key;
+    const entry = await db.collection('models').findOne({ key: model_key });
+    await uploadToS3(file.path, entry.uuid);
+    return res.json({"a": "b"});
+});
+
+
 setup().then(() => {
-    post();
     server.listen(3000, () => {
         console.log("Listening on the port 3000...");
     });
-})
-
-server.get('/', async (req, res) => {
-    const whatever = await db.collection('models').findOne({name:"test"})
-    res.send(whatever);
 });
-
-server.post('/', async (req, res) => {
-    const collection = await db.collection('models')
-    const request = req.json();
-    if (request.name == null || request.input == null || request.input.type == null || request.input.size == null) {
-        console.log("req contains NULL values");
-    }
-    const new_record = generateRecord(request.name, request.input);
-    collection.insertOne(new_record);
-});
-
-
-
